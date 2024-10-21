87812518f7ccc9dd692bbc789d63794793bf012c
nguyenjh <122562684+nguyenjh@users.noreply.github.com>
2024-10-21 14:46:07 -0700

    Exercise 1 done

--- src/components/Expense/AddExpenseForm.tsx
+++ src/components/Expense/AddExpenseForm.tsx
@@ -1,13 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { AppContext } from "../../context/AppContext";
+import ExpenseList from "./ExpenseList";
+import { Expense } from "../../types/types";
+
+const expenseInitialState: Expense = {
+    id: "",
+    name: "",
+    cost: 0,
+}
+
 const AddExpenseForm = () => {
   // Exercise: Consume the AppContext here
+  const context = useContext(AppContext);
 
   // Exercise: Create name and cost to state variables
+  const[expense, setExpense] = useState(expenseInitialState)
+  const[name, setName] = useState("");
+  const handleNameInput = (name: string) => {
+    setName(name);
+    setExpense({ ...expense, id: name, name: name });
+  }
+  const[cost, setCost] = useState("0");
+  const handleCostInput = (cost: string) => {
+    setCost(cost);
+    setExpense({ ...expense, cost: parseInt(cost)})
+  }
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     // Exercise: Add add new expense to expenses context array
+    context.setExpenses(context.expenses.concat(expense));
   };
 
   return (
@@ -20,8 +43,10 @@ const AddExpenseForm = () => {
             type="text"
             className="form-control"
             id="name"
-            value={""}
+            value={name}
             // HINT: onChange={}
+            onChange={(event) =>
+              handleNameInput(event.target.value)}
           ></input>
         </div>
         <div className="col-sm">
@@ -31,8 +56,10 @@ const AddExpenseForm = () => {
             type="text"
             className="form-control"
             id="cost"
-            value={0}
+            value={cost}
             // HINT: onChange={}
+            onChange={(event) =>
+              handleCostInput(event.target.value)}
           ></input>
         </div>
         <div className="col-sm">
