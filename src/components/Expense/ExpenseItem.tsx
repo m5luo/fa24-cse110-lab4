ff1662b0d3d3f8abf47fc9ee7028c26cebe1ede2
m5luo <m5luo@ucsd.edu>
2024-10-21 14:40:53 -0700

    Exercise 1 Done

--- src/components/Expense/ExpenseItem.tsx
+++ src/components/Expense/ExpenseItem.tsx
@@ -1,10 +1,17 @@
+import React, { useState, useContext } from "react";
 import { Expense } from "../../types/types";
+import { AppContext } from "../../context/AppContext";
 
 const ExpenseItem = (currentExpense: Expense) => {
   // Exercise: Consume the AppContext here
+  const context = useContext(AppContext);
 
   const handleDeleteExpense = (currentExpense: Expense) => {
     // Exercise: Remove expense from expenses context array
+    console.log(context.expenses)
+    context.setExpenses(() => {
+        return context.expenses.filter((expense) => expense.id !== currentExpense.id);
+    })
   };
 
   return (
