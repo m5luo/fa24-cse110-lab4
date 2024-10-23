799c4b85d4ffff7398e8b78737fba6f89b3f12bc
nguyenjh <122562684+nguyenjh@users.noreply.github.com>
2024-10-23 00:54:45 -0700

    Exercise 3 done

--- src/components/Remaining.tsx
+++ src/components/Remaining.tsx
@@ -1,9 +1,8 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../context/AppContext";
 
 const Remaining = () => {
-  const { expenses } = useContext(AppContext);
-  let budget = 1000;
+  const { expenses, budget } = useContext(AppContext);
 
   const totalExpenses = expenses.reduce((total, item) => {
     return (total = total + item.cost);
@@ -12,8 +11,8 @@ const Remaining = () => {
   const alertType = totalExpenses > budget ? "alert-danger" : "alert-success";
 
   // Exercise: Create an alert when Remaining is less than 0.
-  if (alertType === "alert-danger") {
-    alert("You have exceeded your budget!");
+  if (totalExpenses > budget) {
+    alert("You have exceeded your budget!")
   }
 
   return (
