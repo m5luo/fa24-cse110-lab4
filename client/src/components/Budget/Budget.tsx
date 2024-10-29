95a32b53914a534c42771091c3296526d6d97425
m5luo <m5luo@ucsd.edu>
2024-10-29 13:16:11 -0700

    Exercise 2,3 Done

--- client/src/components/Budget/Budget.tsx
+++ client/src/components/Budget/Budget.tsx
@@ -1,24 +1,43 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AppContext } from "../../context/AppContext";
+import { fetchBudget, updateBudget } from "../../utils/budget-utils";
 
 const Budget = () => {
     const context = useContext(AppContext);
-    const budget = context.budget;
     
-    const [updateBudget, setUpdateBudget] = useState(budget.toString())
+    // Fetch budget on component mount
+    useEffect(() => {
+        loadBudget();
+    }, []);
+
+    // Function to load budget and handle errors
+    const loadBudget = async () => {
+        try {
+        const budget = await fetchBudget();
+        context.setBudget(budget);
+        } catch (err: any) {
+        console.log(err.message);
+        }
+    };
+
+    const [inputBudget, setInputBudget] = useState(context.budget.toString())
+    console.log(context.budget)
 
     const handleBudgetUpdate = (newBudget: string) => {
-        setUpdateBudget(newBudget)
+        setInputBudget(newBudget)
     }
 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        context.setBudget(parseInt(updateBudget))
+        console.log(context.budget)
+
+        updateBudget(parseInt(inputBudget))
+        context.setBudget(parseInt(inputBudget))
     };
     
   return (
     <div className="alert alert-secondary p-3 d-flex align-items-center justify-content-between">
-        <div>Budget: ${budget}</div>
+        <div>Budget: ${context.budget}</div>
         <form onSubmit={(event) => onSubmit(event)}>
         <div className="col-sm">
         <label htmlFor="budget">Update Budget</label>
@@ -27,7 +46,7 @@ const Budget = () => {
                 type="number"
                 className="form-control"
                 id="budget"
-                value={updateBudget}
+                value={inputBudget}
                 onChange={(event) =>
                      handleBudgetUpdate(event.target.value)}
             ></input>
