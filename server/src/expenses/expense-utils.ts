1cfac59871ce9edf910e01e2efde44fb6044e07d
m5luo <m5luo@ucsd.edu>
2024-11-04 14:38:53 -0800

    Exercise 1, 2 Done

--- server/src/expenses/expense-utils.ts
+++ server/src/expenses/expense-utils.ts
@@ -1,24 +1,45 @@
 import { Expense } from "../types";
 import { Request, Response } from "express";
+import { Database } from "sqlite";
 
-export function createExpenseServer(req: Request, res: Response, expenses: Expense[]) {
-    const { id, cost, description } = req.body;
+// export function createExpenseServer(req: Request, res: Response, expenses: Expense[]) {
+//     const { id, cost, description } = req.body;
 
-    if (!description || !id || !cost) {
-        return res.status(400).send({ error: "Missing required fields" });
-    }
+//     if (!description || !id || !cost) {
+//         return res.status(400).send({ error: "Missing required fields" });
+//     }
 
-    const newExpense: Expense = {
-        id: id,
-        description,
-        cost,
+//     const newExpense: Expense = {
+//         id: id,
+//         description,
+//         cost,
+//     };
+
+//     expenses.push(newExpense);
+//     res.status(201).send(newExpense);
+// }
+
+export async function createExpenseServer(req: Request, res: Response, db: Database) {
+    
+    try {
+        // Type casting the request body to the expected format.
+        const { id, cost, description } = req.body as { id: string, cost: number, description: string };
+ 
+        if (!description || !id || !cost) {
+            return res.status(400).send({ error: "Missing required fields" });
+        }
+ 
+        await db.run('INSERT INTO expenses (id, description, cost) VALUES (?, ?, ?);', [id, description, cost]);
+        res.status(201).send({ id, description, cost });
+ 
+    } catch (error) {
+        return res.status(400).send({ error: `Expense could not be created, + ${error}` });
     };
+ 
+ }
+ 
 
-    expenses.push(newExpense);
-    res.status(201).send(newExpense);
-}
-
-export function deleteExpense(req: Request, res: Response, expenses: Expense[]) {
+export async function deleteExpense(req: Request, res: Response, db: Database) {
     // TO DO: Implement deleteExpense function
     const { id } = req.params;
 
@@ -26,42 +47,33 @@ export function deleteExpense(req: Request, res: Response, expenses: Expense[]) 
         return res.status(400).send({ error: "Missing required fields" });
     }
 
-    const index = expenses.findIndex(expense => expense.id === id);
-    console.log("deleted",index)
+    try {
+        await db.run('DELETE FROM expenses WHERE id=?;', [id]);
+    } catch (error) {
+        return res.status(400).send({ error: `Expense could not be created, + ${error}` });
+    };
+
+    // const index = expenses.findIndex(expense => expense.id === id);
+    // console.log("deleted",index)
     
-    if (index === -1) {
-        return res.status(404).send({ error: "Expense index not found" });
-    }
-    // const [deletedExpense] = expenses.splice(index, 1);
-
-    expenses = expenses.splice(index,1);
-    console.log(JSON.stringify(expenses));
-    return res.status(200).send(expenses);
-
-
-    // const expenseToRemoveID = id;
-    // const updatedExpensesList = expenses.filter((expense) => expense.id !== expenseToRemoveID);
-    // const indexToRemove = expenses.indexOf(expenseToRemoveID);
-    
-    // if (indexToRemove > -1) {
-    //     expenses.splice(indexToRemove, 1);
+    // if (index === -1) {
+    //     return res.status(404).send({ error: "Expense index not found" });
     // }
-    
-    // const updatedExpensesList = expenses.filter(id => oldExpense.id);
-    // expenses = updatedExpensesList;
+    // // const [deletedExpense] = expenses.splice(index, 1);
 
-    
-    // if (index > -1) {
-    //     // expenses.splice(index, 1);
-    //     const [deletedExpense] = expenses.splice(index, 1);
-    //     return res.status(200).send(deletedExpense);
-    // }
-    
-    // return res.status(404).send({ error: "Expense not found" });
-    // const updatedStrings = deleteStringById(strings, "cherry");
-    // res.status(201).send(expenses);
+    // expenses = expenses.splice(index,1);
+    // console.log(JSON.stringify(expenses));
+    // return res.status(200).send(expenses);
 }
 
-export function getExpenses(req: Request, res: Response, expenses: Expense[]) {
-    res.status(200).send({ "data": expenses });
+export async function getExpenses(req: Request, res: Response, db: Database) {
+    // res.status(200).send({ "data": expenses });
+    try {
+        // await db.get('SELECT description, cost FROM expenses;');
+        let rows = await db.all('SELECT description, cost FROM expenses;');
+        res.status(201).send({"data":rows});
+    } catch (error) {
+        return res.status(400).send({ error: `Expense could not be get, + ${error}` });
+    };
+
 }
